fe472c1bcf997ed850175c4d8ace03e4897cd232
abaicus <baicusandrei@me.com>
2023-01-10 15:15:16 +0200

    fix: markup and structure for patterns, parts & templates

--- inc/patterns/row-with-logos.php
+++ inc/patterns/row-with-logos.php
@@ -14,44 +14,34 @@
 
 use Jaxon\Assets_Manager;
 
+$logos = array(
+	Assets_Manager::get_image_url( 'jaxon-placeholder-logo-05.1.2.svg' ),
+	Assets_Manager::get_image_url( 'jaxon-placeholder-logo-04.1.2.svg' ),
+	Assets_Manager::get_image_url( 'jaxon-placeholder-logo-01.1.2.svg' ),
+	Assets_Manager::get_image_url( 'jaxon-placeholder-logo-02.1.2.svg' ),
+	Assets_Manager::get_image_url( 'jaxon-placeholder-logo-03.1.2.svg' ),
+);
 
+$logo_markup = '
+<!-- wp:image {"align":"center","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="%s"/></figure>
+<!-- /wp:image -->
+';
+
+$logos_markup = '';
+
+foreach ( $logos as $logo ) {
+	$logos_markup .= sprintf( $logo_markup, esc_url( $logo ) );
+}
 
 return array(
 	'title'      => __( 'Row with logos', 'jaxon' ),
 	'categories' => array( 'jaxon' ),
 	'content'    => '
-    <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"40px","bottom":"40px","right":"0"},"blockGap":"var:preset|spacing|80","margin":{"top":"0px","bottom":"0px"}}},"backgroundColor":"ti-accent","textColor":"ti-fg-alt","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-ti-fg-alt-color has-ti-accent-background-color has-text-color has-background" style="margin-top:0px;margin-bottom:0px;padding-top:40px;padding-right:0;padding-bottom:40px"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"width":134,"height":35,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="' . esc_url( Assets_Manager::get_image_url( 'jaxon-placeholder-logo-05.1.2.svg' ) ) . '" width="134" height="35"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"width":108,"height":27,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="' . esc_url( Assets_Manager::get_image_url( 'jaxon-placeholder-logo-04.1.2.svg' ) ) . '" width="108" height="27"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"width":140,"height":36,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="' . esc_url( Assets_Manager::get_image_url( 'jaxon-placeholder-logo-01.1.2.svg' ) ) . '" width="140" height="36"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"width":149,"height":45,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="' . esc_url( Assets_Manager::get_image_url( 'jaxon-placeholder-logo-02.1.2.svg' ) ) . '" width="149" height="45"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"width":137,"height":36,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="' . esc_url( Assets_Manager::get_image_url( 'jaxon-placeholder-logo-03.1.2.svg' ) ) . '" width="137" height="36"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"40px","right":"var:preset|spacing|20","bottom":"40px","left":"var:preset|spacing|20"}}},"backgroundColor":"ti-accent","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-ti-accent-background-color has-background" style="padding-top:40px;padding-right:var(--wp--preset--spacing--20);padding-bottom:40px;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide">' . $logos_markup . '</div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 ',
 );
